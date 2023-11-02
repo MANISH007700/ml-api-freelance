0b1753859dd7f8094d32e58c557aa5a35a2298af
MANISH007700 <monalmonal007@gmail.com>
2023-11-02 17:38:40 +0530

    Update : paraphrase hp

--- app/utils.py
+++ app/utils.py
@@ -33,27 +33,29 @@ def paraphrase_text(model, tokenizer, device, input_text, num_return_sequences=1
 
     outputs = model.generate(
         input_ids=input_ids, attention_mask=attention_masks,
+        max_new_tokens=torch.sum(attention_masks == 1).item()+15,
         max_length=256,
         do_sample=True,
         num_beams=5,
         # top_k=120,
-        temperature=1.5,
-        top_p=1,
+        temperature=3,
+        top_p=0.95,
         num_return_sequences=num_return_sequences
     )
     paraphrashed_text = tokenizer.decode(outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
     return paraphrashed_text
 
 def correct_grammar(model, tokenizer, device, input_text, num_return_sequences=1):
-    text =  input_text + '.'
+    text =  input_text
 
     encoding = tokenizer.encode_plus(text, return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
     outputs = model.generate(
         input_ids=input_ids, 
         attention_mask=attention_masks,
-        max_new_tokens=torch.sum(attention_masks == 1).item()+5,
+        max_new_tokens=torch.sum(attention_masks == 1).item()+15,
         num_beams = 5,
+        temperature=0.2,
         num_return_sequences=num_return_sequences
     )
 
